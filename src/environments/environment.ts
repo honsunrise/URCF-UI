fbba1b08b564c73f4876bfcbaffdc12904c7b428
zhsyourai <mysoftzhs@gmail.com>
2018-05-08 13:59:22 +0800

    Upgrade to @angular@6.0.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,10 +13,18 @@ export const environment = {
 export const APP_DI_CONFIG: IAppConfig = {
   loginEndpoint: 'http://127.0.0.1:8080/v1/uaa',
   logEndpoint: 'http://127.0.0.1:8080/v1/log',
-  pluginEndpoint: 'http://127.0.0.1:8080/v1/plugin',
+  pluginEndpoint: 'http://127.0.0.1:8080/v1/plugins',
   websEndpoint: 'http://127.0.0.1:8080/v1/webs',
   uploadEndpoint: 'http://127.0.0.1:8080',
   requestRetry: 1,
   uploadRetry: 1,
   uploadChunkRetry: 3
 };
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
